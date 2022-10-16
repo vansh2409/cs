835690dfcb32b5b693f6e45cdbf760d68b1d2e29
vansh2409 <75140413+vansh2409@users.noreply.github.com>
2022-10-16 22:53:38 +0530

    3.1

--- src/components/footer.js
+++ src/components/footer.js
@@ -105,7 +105,7 @@ const Footer = () => {
       </StyledSocialLinks>
 
       <StyledCredit tabindex="-1">
-          <div>Designed & Built by \n Arsh, Hilal & Vansh</div>
+          <div>Designed & Built by Arsh, Hilal & Vansh</div>
       </StyledCredit>
     </StyledFooter>
   );
