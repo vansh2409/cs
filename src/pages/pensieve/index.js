c1dbf02963df975b24d41311d43ac31537b7e656
vansh2409 <75140413+vansh2409@users.noreply.github.com>
2022-10-16 23:41:09 +0530

    4.99

--- src/pages/pensieve/index.js
+++ src/pages/pensieve/index.js
@@ -5,6 +5,7 @@ import PropTypes from 'prop-types';
 import { Helmet } from 'react-helmet';
 import styled from 'styled-components';
 import { Layout } from '@components';
+import { IconBookmark } from '@components/icons';
 
 const StyledMainContainer = styled.main`
   & > header {
